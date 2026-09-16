0189fbc150386a8837ff0bed5faa2539b87c8b98
Kristupas Arifovas <50778281+Quikmove@users.noreply.github.com>
2026-09-17 03:14:56 +0800

    FOC-14 chore: add missing decorator to user repository

--- user-service/src/main/java/com/cs3219/foc/user/repository/UserRepository.java
+++ user-service/src/main/java/com/cs3219/foc/user/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.cs3219.foc.user.repository;
 import com.cs3219.foc.user.model.entity.User;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByEmail(String email);
 }
