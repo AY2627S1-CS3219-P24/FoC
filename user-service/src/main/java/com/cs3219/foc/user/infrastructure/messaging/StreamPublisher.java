2fc09f93a60af7d45ad302e7240b5c899773724d
Kristupas Arifovas <50778281+Quikmove@users.noreply.github.com>
2026-09-17 21:23:16 +0800

    FOC-16: Configure RabbitMQ messaging
    # Conflicts:
    #	credit-service/build.gradle
    #	order-service/build.gradle
    #	supplier-service/build.gradle

---
+++ user-service/src/main/java/com/cs3219/foc/user/infrastructure/messaging/StreamPublisher.java
@@ -0,0 +1,15 @@
+package com.cs3219.foc.user.infrastructure.messaging;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StreamPublisher {
+    private final StreamBridge streamBridge;
+
+    public void publish(String bindingName, Object payload) {
+        streamBridge.send(bindingName, payload);
+    }
+}
